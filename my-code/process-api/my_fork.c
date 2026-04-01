80d165482ff1900b7fa1fc1f86eccff3f3ca563a
fufu <roycelang24@gamil.com>
2026-04-01 15:30:01 +0800

    fork api

--- my-code/process-api/my_fork.c
+++ my-code/process-api/my_fork.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * fork api的作用是拷贝当前父进程的所有状态到一个子进程
+ * 但是fork的返回值和父进程不一样:
+ *  - 父进程中返回子进程的pid
+ *  - 子进程中返回0
+ */
+
 int main(int argc, char* argv[]) {
     printf("Hello, World! %d\n", (int)getpid());
     int pid = fork();
